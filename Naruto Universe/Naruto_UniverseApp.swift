3068a70cae694557324e4e28dfbf53a79bd159ad
Mohd Kashif <kashifmd123@gmail.com>
2024-04-20 21:09:04 +0530

    initial setup include app icon, launchscreen

---
+++ Naruto Universe/Naruto_UniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Naruto_UniverseApp.swift
+//  Naruto Universe
+//
+//  Created by Mohd Kashif on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Naruto_UniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
